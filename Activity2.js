99a1fac1bef622241b8dc4e29ff7eed7fbe261ff
VanshikaCodes <153213173+VanshikaCodes@users.noreply.github.com>
2024-07-21 17:03:02 +0000

    Day 2

---
+++ Activity2.js
@@ -0,0 +1,12 @@
+const books = {
+     title: 'Book One',
+     author: 'Chanakya',
+     publish: 1981,
+     edition: 2004 
+};
+
+books.info = () => {
+    console.log(books.author, books.title);
+}
+
+console.log(info())
---
+++ Day 7 Objects/Activity1.js
@@ -0,0 +1,10 @@
+const books = {
+    title: 'Book One',
+     genre: 'Fiction',
+      publish: 1981,
+     edition: 2004 
+};
+
+  console.log(books);
+  console.log(books.edition);
+  console.log(books.title);
